b2762088d337ea739bcabb4d5f4cfa1b0c73c524
p <p@121.129.197.165>
2019-06-29 12:45:27 +0900

    gdgd

---
+++ springbootTest/src/main/java/com/Springboot/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.Springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/*
+ * resource setting 
+ */
+@Configuration
+@EnableWebMvc
+@EnableJpaRepositories(basePackages = "com.Springboot.Repository")
+public class AppConfig extends WebMvcConfigurerAdapter {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return null;
+		
+	}
+}
---
+++ springbootTest/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"    pageEncoding="UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix ="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+	<h2>home.jsp</h2>
+	<hr>
+	<div>message : ${messeage}</div>
+	<c:if test="${pageContext.request.userPrincipal.name != null }">
+		welcome ${pageContext.request.userPrincipal.name }!<br>
+		
+		<form:form action="${pageContext.request.contextPath}/logout" method="POST">
+			<input type="submit" value="Logout" />
+		</form:form>
+	</c:if>
+	<hr>
+	<c:if test="${pageContext.request.userPrincipal.name ==null }">
+		<a href ="<c:url value ="/login" />">login</a>
+	</c:if>
+</body>
+</html>
