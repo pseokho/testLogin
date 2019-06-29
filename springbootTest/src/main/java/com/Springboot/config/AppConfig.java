96eee455580127baea9f8a6164aa2824c0e94847
p <p@121.129.197.165>
2019-06-29 13:09:18 +0900

    repostory 까지

--- springbootTest/src/main/java/com/Springboot/config/AppConfig.java
+++ springbootTest/src/main/java/com/Springboot/config/AppConfig.java
@@ -8,8 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-/*
- * resource setting 
+/**
+ * @brief 서버세팅
+ * @author p
+ *
  */
 @Configuration
 @EnableWebMvc
